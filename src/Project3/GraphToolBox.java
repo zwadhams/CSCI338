33865d6e05a9080eca3b756e60d5f61cfe6e4a06
Zach Wadhams <CobaltBomber@gmail.com>
2022-12-03 17:15:21 -0700

    works on inexactVC

--- src/Project3/GraphToolBox.java
+++ src/Project3/GraphToolBox.java
@@ -1,10 +1,19 @@
 package Project3;
 
+import java.util.Arrays;
+
 /**
  *
  * @author yaw
  */
 public class GraphToolBox {
+
+    public static boolean isVC(int[][] graphToBeTested) {
+
+
+        return null;
+    }
+
     // return an array containing the vertex numbers of an optimal VC.
     public static int[] exactVC(Graph inputGraph) {
 
@@ -16,10 +25,33 @@ public class GraphToolBox {
     public static int[] inexactVC(Graph inputGraph) {
         int[] vertexNumbers;
 
-        int[][] workingGraph = inputGraph.getGraph();
+        int[][] originalGraph = inputGraph.getGraph(); //adjacency matrix
+        int numVert = originalGraph.length;
 
-        for (int i = 0; i < 5; i++) {
-            System.out.println(workingGraph[i][0]);
+        //testing stuff
+        System.out.println(Arrays.toString(originalGraph[0]));
+        System.out.println(Arrays.toString(originalGraph[1]));
+        System.out.println(originalGraph[0].length);
+        System.out.println(originalGraph[0][0]);
+
+        System.out.println("Is the original graph a vertex cover?");
+        System.out.println(isVC(originalGraph));
+
+        //actual logic
+        int[][] tempGraph = originalGraph;
+        //gonna randomly remove a row and check if it is a vertex cover
+        for (int i = 0; i < originalGraph.length; i++) {
+            //if (isVC(tempGraph)) {
+
+
+                //return vertexNumbers;
+            //}
+        }
+
+        for (int i = 0; i < originalGraph.length; i++) { //number of rows
+            for (int j = 0; j < originalGraph[i].length; j++) { //number of columns in a row (neighbors)
+
+            }
         }
 
 
@@ -38,6 +70,7 @@ public class GraphToolBox {
     }
 
 
+
     //Running the stuff down here
 
     public static void main(String[] args) {
@@ -45,7 +78,7 @@ public class GraphToolBox {
         Graph graph1 = new Graph("graph1.txt");
 
         int[] inexactVCAnswer = inexactVC(graph1);
-
+        System.out.println("All vertices of the vertex cover:");
         System.out.println(inexactVCAnswer);
     }
 
