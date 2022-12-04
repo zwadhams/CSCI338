77f3dfd7e4d5e4e3ea62595fc26f67dbdba0afd9
Zach Wadhams <CobaltBomber@gmail.com>
2022-12-04 15:40:15 -0700

    works on inexactVC

--- src/Project3/GraphToolBox.java
+++ src/Project3/GraphToolBox.java
@@ -8,22 +8,45 @@ import java.util.Arrays;
  */
 public class GraphToolBox {
 
+    //Should be working, gotta test it some more
     public static boolean isVC(int[][] graphToBeTested) {
+        int graphSize = graphToBeTested.length;
 
+        //gets size
+        boolean visited[] = new boolean[graphSize]; //gets size
 
-        return null;
+        //sets all to false by default
+        for (int i = 0; i < graphSize; i++) {
+            visited[i] = false;
+        }
+        //checks all edges and marks vertex if visited
+        for (int u = 0; u < graphSize; u++) {
+            if (visited[u] == false) {
+                for (int j = 0 ; j < graphToBeTested[u].length; j++) {
+                    visited[graphToBeTested[u][j]] = true;
+                    visited[u] = true;
+                }
+            }
+        }
+        //checks if there are any false in visited, if so, it is not a vertex cover
+        for (int i = 0; i < visited.length; i++) {
+            if (visited[i] == false) {
+                return false;
+            }
+        }
+        //if it passes everything, it is a vertex cover
+        return true;
     }
 
     // return an array containing the vertex numbers of an optimal VC.
     public static int[] exactVC(Graph inputGraph) {
-
 
         return null;
     }
     
     // return (in polynomial time) an array containing the vertex numbers of a VC.
     public static int[] inexactVC(Graph inputGraph) {
-        int[] vertexNumbers;
+
 
         int[][] originalGraph = inputGraph.getGraph(); //adjacency matrix
         int numVert = originalGraph.length;
@@ -39,22 +62,20 @@ public class GraphToolBox {
 
         //actual logic
         int[][] tempGraph = originalGraph;
+
+
+        //checking for base vertex cover functionality
         //gonna randomly remove a row and check if it is a vertex cover
         for (int i = 0; i < originalGraph.length; i++) {
-            //if (isVC(tempGraph)) {
+            if (isVC(tempGraph) == true) {
+                int[] vertexNumbers = new int[tempGraph.length];
+                for (int j = 0; j < tempGraph.length; j++) {
+                    vertexNumbers[j] = tempGraph[i][j];
+                }
 
-
-                //return vertexNumbers;
-            //}
-        }
-
-        for (int i = 0; i < originalGraph.length; i++) { //number of rows
-            for (int j = 0; j < originalGraph[i].length; j++) { //number of columns in a row (neighbors)
-
+                return vertexNumbers;
             }
         }
-
-
 
         return null;
     }
@@ -73,13 +94,14 @@ public class GraphToolBox {
 
     //Running the stuff down here
 
+
     public static void main(String[] args) {
 
         Graph graph1 = new Graph("graph1.txt");
 
         int[] inexactVCAnswer = inexactVC(graph1);
-        System.out.println("All vertices of the vertex cover:");
-        System.out.println(inexactVCAnswer);
+        System.out.println("Number of vertices in the inexact vertex cover: ");
+        System.out.println(inexactVCAnswer.length);
     }
 
 }
