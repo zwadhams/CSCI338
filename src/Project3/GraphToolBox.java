a65fe292e1d786d954cb874db44a1fb202b23f51
Zach Wadhams <CobaltBomber@gmail.com>
2022-12-04 17:04:58 -0700

    works on inexactVC

--- src/Project3/GraphToolBox.java
+++ src/Project3/GraphToolBox.java
@@ -9,8 +9,8 @@ import java.util.Arrays;
 public class GraphToolBox {
 
     //Should be working, gotta test it some more
-    public static boolean isVC(int[][] graphToBeTested) {
-        int graphSize = graphToBeTested.length;
+    public static boolean isVC(int[][] originGraph, int startingVertex) {
+        int graphSize = originGraph.length;
 
         //gets size
         boolean visited[] = new boolean[graphSize]; //gets size
@@ -20,14 +20,32 @@ public class GraphToolBox {
             visited[i] = false;
         }
         //checks all edges and marks vertex if visited
-        for (int u = 0; u < graphSize; u++) {
+        //working code below
+
+        /*for (int u = 0; u < graphSize; u++) {
             if (visited[u] == false) {
-                for (int j = 0 ; j < graphToBeTested[u].length; j++) {
-                    visited[graphToBeTested[u][j]] = true;
-                    visited[u] = true;
+                for (int j = 0; j < originGraph[u].length; j++) {
+                    if (originGraph[u] != null) {
+                        visited[originGraph[u][j]] = true;
+                        visited[u] = true;
+                    }
+                }
+            }
+        }*/
+
+        for (int u = startingVertex; u < graphSize - startingVertex; u++) {
+            if (visited[u] == false) {
+                for (int j = 0; j < originGraph[u].length; j++) {
+                    if (originGraph[u] != null) {
+                        visited[originGraph[u][j]] = true;
+                        visited[u] = true;
+                    }
                 }
             }
         }
+
+        System.out.println(Arrays.toString(visited));
+
         //checks if there are any false in visited, if so, it is not a vertex cover
         for (int i = 0; i < visited.length; i++) {
             if (visited[i] == false) {
@@ -53,20 +71,31 @@ public class GraphToolBox {
 
         //testing stuff
         System.out.println(Arrays.toString(originalGraph[0]));
-        System.out.println(Arrays.toString(originalGraph[1]));
         System.out.println(originalGraph[0].length);
+        System.out.println(Arrays.toString(originalGraph[1]));
+        System.out.println(originalGraph[1].length);
         System.out.println(originalGraph[0][0]);
 
         System.out.println("Is the original graph a vertex cover?");
-        System.out.println(isVC(originalGraph));
+        System.out.println(isVC(originalGraph, 0));
+        System.out.println("Reducing the graph to see if the next is a cover");
+
+        int[][] tempGraph = originalGraph;
+
+        System.out.println("Trying to see if this will fail");
+        System.out.println(isVC(originalGraph, 7));
+
+        //System.out.println(Arrays.toString(tempGraph[0]));
+        //System.out.println(Arrays.toString(tempGraph[1]));
 
         //actual logic
-        int[][] tempGraph = originalGraph;
+
 
 
         //checking for base vertex cover functionality
         //gonna randomly remove a row and check if it is a vertex cover
-        for (int i = 0; i < originalGraph.length; i++) {
+        //DONT REMOVE A ROW, just set it to be empty
+        /*for (int i = 0; i < originalGraph.length; i++) {
             if (isVC(tempGraph) == true) {
                 int[] vertexNumbers = new int[tempGraph.length];
                 for (int j = 0; j < tempGraph.length; j++) {
@@ -75,7 +104,7 @@ public class GraphToolBox {
 
                 return vertexNumbers;
             }
-        }
+        }*/
 
         return null;
     }
