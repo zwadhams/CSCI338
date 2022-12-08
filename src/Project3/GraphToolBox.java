b2d60ad1b0891dae459b36e37ea6249178ce96d4
ZachWadhams <zachary.wadhams@student.montana.edu>
2022-12-08 13:47:32 -0700

    makes exactVC() work sort of right

--- src/Project3/GraphToolBox.java
+++ src/Project3/GraphToolBox.java
@@ -44,7 +44,6 @@ public class GraphToolBox {
         return true;
     }
 
-    //needs work
     public static boolean isIS(int[][] originGraph, ArrayList<Integer> listOfIgnoredVertices) {
         int graphSize = originGraph.length;
 
@@ -150,7 +149,6 @@ public class GraphToolBox {
 
         int[] arr = ALvertexVC.stream().mapToInt(i -> i).toArray();
 
-
         long endTime = System.nanoTime();
         System.out.println("Took this long: " + (endTime-startTime));
         return arr;
@@ -158,13 +156,51 @@ public class GraphToolBox {
 
     // return an array containing the vertex numbers of an optimal IS.
     public static int[] optimalIS(Graph inputGraph) {
-        //make it run for a super long by forcing a exponential time maybe?
         int[][] originalGraph = inputGraph.getGraph(); //adjacency matrix
-
 
         ArrayList<Integer> listOfIgnoredVertices = new ArrayList<Integer>();
 
-        return null;
+        //power set implementation
+        int[][] powerSet = new int[originalGraph.length * originalGraph.length][originalGraph.length];
+
+        //fill our ignoredVertices
+        for (int i = 0; i < originalGraph.length; i++) {
+            listOfIgnoredVertices.add(i);
+        }
+
+        int brokeOn = 0;
+        //gets largest independent by adding node 1 (by removing from ignored) and so forth until it is not an independent set
+        for (int i = originalGraph.length - 1; i > -1; i--) {
+            listOfIgnoredVertices.remove(i);
+            boolean isIndependentSet = isIS(originalGraph, listOfIgnoredVertices);
+            if (!isIndependentSet) {
+                brokeOn = i;
+                break;
+            }
+            for (int k = 0; k < powerSet.length; k++) {
+                for (int j = 0; j < originalGraph.length; j++) {
+                    if (powerSet.length == listOfIgnoredVertices.get(k)) {
+                        brokeOn = i;
+                        break;
+                    }
+                    else {
+                        listOfIgnoredVertices.remove(i);
+                    }
+                }
+            }
+        }
+
+        ArrayList<Integer> ALvertexIS = new ArrayList<Integer>();
+
+        for (int i = 0; i < originalGraph.length; i++) {
+            if (!listOfIgnoredVertices.contains(i) && i != brokeOn) {
+                ALvertexIS.add(i);
+            }
+        }
+
+        int[] arr = ALvertexIS.stream().mapToInt(i -> i).toArray();
+
+        return arr;
     }
 
     // return (in polynomial time) an array containing the vertex numbers of a IS.
@@ -179,7 +215,6 @@ public class GraphToolBox {
             listOfIgnoredVertices.add(i);
         }
 
-        //testing
         int brokeOn = 0;
         //gets largest independent by adding node 1 (by removing from ignored) and so forth until it is not an independent set
         for (int i = originalGraph.length - 1; i > -1; i--) {
@@ -211,16 +246,6 @@ public class GraphToolBox {
         Graph simpleGraph = new Graph("simple.txt");
         Graph simple10 = new Graph("simple10.txt");
 
-        /*System.out.println("Simple graph testing");
-        System.out.println("---vertex simple---");
-        int [] simpleVC = inexactVC(simpleGraph);
-        System.out.println(Arrays.toString(simpleVC));
-        System.out.println("---ind set simple---");
-        int [] simpleIS = inexactIS(simpleGraph);
-        System.out.println(Arrays.toString(simpleIS));
-
-         */
-
         Graph graph1 = new Graph("graph1.txt");
 
         System.out.println("------------Exact Vertex Cover------------");
@@ -238,15 +263,14 @@ public class GraphToolBox {
         System.out.println("-------------------------------------------");
 
         System.out.println("----------Optimal Independent Set----------");
-        //To be implemented
-        int[] optimalISAnswer = optimalIS(simpleGraph);
+        int[] optimalISAnswer = optimalIS(simple10);
         System.out.println("List of vertices in independent set:");
         System.out.println(Arrays.toString(optimalISAnswer));
         System.out.println("Number of vertices in the inexact independent set: " + optimalISAnswer.length);
         System.out.println("-------------------------------------------");
 
         System.out.println("----------Inexact Independent Set----------");
-        int[] inexactISAnswer = inexactIS(simpleGraph);
+        int[] inexactISAnswer = inexactIS(simple10);
         System.out.println("List of vertices in independent set:");
         System.out.println(Arrays.toString(inexactISAnswer));
         System.out.println("Number of vertices in the inexact independent set: " + inexactISAnswer.length);
